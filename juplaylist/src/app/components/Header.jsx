def93c702c18b6e2567404fd18a226ddf2153320
ronfl3x <jupitr.gabriel@gmail.com>
2022-11-02 11:51:45 +0100

    header done, navigation's missing

--- juplaylist/src/app/components/Header.jsx
+++ juplaylist/src/app/components/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import "../css/Header.css";
+import { FiUser } from "react-icons/fi";
+import logo from '../assets/logo/logo.svg';
+
+
 
 function Header(props) {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <nav>
+                    <a href="/#" className="logo header-element">
+                        <img src={logo} alt="logo" />
+                    </a>                    
+                <div className='name header-element'>
+                    <h1>Juplaylist</h1>
+                </div>
+                    <a href="/#" className="user header-element">
+                        <FiUser className='user-icon'/>
+                    </a>   
+            </nav>
+        </header>
     );
 }
 
