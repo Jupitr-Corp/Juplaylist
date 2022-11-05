912a710b98c1124403da8730989b219e4f6ee7da
ronfl3x <jupitr.gabriel@gmail.com>
2022-11-05 22:47:50 +0100

    quick js fix

--- juplaylist/src/app/ux/Jupop.jsx
+++ juplaylist/src/app/ux/Jupop.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FiX } from "react-icons/fi";
 import "../css/Jupop.css";
 
 function Jupop(props) {
   const { message, backgroundColor, visibility, setVisibility } = props;
 
-  if (visibility === "visible") {
-    setTimeout(() => {
-      setVisibility("hidden");
-    }, 3000);
-  }
+  // ------------------ States ------------------
+
+  // ------------------ Effects  ------------------
+  let viAnim;
+  useEffect(() => {
+    if (visibility === "visible") {
+      viAnim = setTimeout(() => {
+        setVisibility("hidden");
+      }, 3000);
+    }
+  }, [visibility, setVisibility]);
 
   return (
     <div
-      className={visibility == "hidden" ? "jupop hidden" : "jupop visible"}
+      className={visibility === "hidden" ? "jupop hidden" : "jupop visible"}
       style={{ backgroundColor: backgroundColor, visibility: visibility }}
     >
       <button
         className="jupop-button"
         onClick={() => {
           setVisibility("hidden");
+          clearTimeout(viAnim);
         }}
       >
         <FiX className="x-icon" color="color" />
