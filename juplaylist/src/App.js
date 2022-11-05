e48f1eedab688e2a352092968149fda1cdf1b78a
Niout <49318861+Niout@users.noreply.github.com>
2022-11-05 18:04:01 +0100

    login page (#6)

--- juplaylist/src/App.js
+++ juplaylist/src/App.js
@@ -5,6 +5,7 @@ import Layout from "./app/pages/Layout";
 import Home from "./app/pages/Home";
 import Account from "./app/pages/Account";
 import NoPage from "./app/pages/NoPage";
+import PhoneLogin from "./app/pages/PhoneLogin";
 
 function App() {
   // ------------------ State ------------------
@@ -37,7 +38,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home platform={platform} />} />
-          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Account />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
--- juplaylist/src/app/pages/Account.jsx
+++ juplaylist/src/app/pages/Account.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React from "react";
+import hands_phone from "../assets/illustrations/Hands-Phone.png";
+import PhoneInput from "../components/PhoneInput";
+import "../css/Account.css";
+import { authentication } from "../components/firebase-config";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 function Account(props) {
-    return (
-        <div>
-            
-        </div>
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+
+  const generateRecaptchaVerifier = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-verifier",
+      {
+        size: "invisible",
+        callback: (response) => {},
+      },
+      authentication
     );
+  };
+
+  function submit() {
+    if (
+      /^(?:[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)\d(?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/.test(
+        phoneNumber
+      )
+    ) {
+      generateRecaptchaVerifier();
+      signInWithPhoneNumber(
+        authentication,
+        "+33" +
+          (phoneNumber.length === 10 ? phoneNumber.slice(1) : phoneNumber),
+        window.recaptchaVerifier
+      )
+        .then((confirmationResult) => {
+          console.log(confirmationResult);
+          window.confirmationResult = confirmationResult;
+        })
+        .catch((error) => {
+          // Error; SMS not sent
+          console.log(error);
+        });
+    }
+  }
+
+  return (
+    <div className="account-container">
+      <div>
+        <img src={hands_phone} alt="hands with phone" />
+      </div>
+      <div className="mobile-container">
+        <h3>Enter your phone number</h3>
+        <p>We will send you the one time pin</p>
+        <PhoneInput code="+33" onTextChange={setPhoneNumber} />
+        <button onClick={() => submit()}>Send</button>
+        <div id="recaptcha-verifier"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Account;
