139961aff817f7179569be47e75247ad9546bc23
Niout <timmerle1204@gmail.com>
2022-11-13 12:51:47 +0100

    Request

--- juplaylist/src/App.js
+++ juplaylist/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./app/css/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "./app/pages/Layout";
 import Home from "./app/pages/Home";
 import Login from "./app/pages/Login";
 import NoPage from "./app/pages/NoPage";
 import PhoneLogin from "./app/pages/PhoneLogin";
+import Request from "./app/components/RequestComponent";
 
 function App() {
   // ------------------ Render ------------------
   return (
-    <>
-      <ToastContainer limit={5} />
+    <Request>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -24,7 +23,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </Request>
   );
 }
 
