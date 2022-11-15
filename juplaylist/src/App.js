7821645977e3858a91e6b22fab5be77986ffd2b1
Niout <49318861+Niout@users.noreply.github.com>
2022-11-15 19:32:35 +0100

    de (#13)

--- juplaylist/src/App.js
+++ juplaylist/src/App.js
@@ -8,6 +8,7 @@ import Login from "./app/pages/Login";
 import NoPage from "./app/pages/NoPage";
 import PhoneLogin from "./app/pages/PhoneLogin";
 import Request from "./app/components/RequestComponent";
+import PrivateRoute from "./app/components/PrivateRoute";
 
 function App() {
   // ------------------ Render ------------------
@@ -16,7 +17,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route
+              index
+              element={
+                <PrivateRoute path="/login">
+                  <Home />
+                </PrivateRoute>
+              }
+            />
             <Route path="login" element={<Login />} />
             <Route path="login/phone" element={<PhoneLogin />} />
             <Route path="*" element={<NoPage />} />
